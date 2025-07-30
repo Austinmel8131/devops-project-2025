2f4b6496ff2bc23bbb171b6de7b2f0aa93ceee07
Austin Odiase <austinmel8131@gmail.com>
2025-07-30 01:44:18 +0100

    project: updated gitaction file to run and check migrations

--- api/models.py
+++ api/models.py
@@ -4,4 +4,4 @@ class Book(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     author = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True) 
